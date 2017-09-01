24778dceb152203ec7d63df3f6981f20746eb6c8
tedcurrent <tetsiiedm@gmail.com>
2017-09-01 12:24:33 +0300

    Compress background and clean up

---
+++ public/main.js
@@ -0,0 +1,51 @@
+function playSound(keyCode) {
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+}
+
+function keyboardPlaySound(e) {
+    return !e.repeat && playSound(e.keyCode);
+}
+
+function mousePlaySound(key, e) {
+    return !e.repeat && key.contains(e.target) && playSound(key.getAttribute("data-key"));
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+
+    this.classList.remove("playing");
+}
+
+const appState = {
+    playbackRate: 1.0,
+    filter: 18000
+};
+
+const context = new (window.AudioContext || window.webkitAudioContext)();
+const filter = context.createBiquadFilter();
+filter.type = "lowpass";
+filter.frequency.value = appState.filter;
+
+const audioInputs = document.querySelectorAll("audio");
+audioInputs.forEach((audio) => {
+    const source = context.createMediaElementSource(audio);
+    source.connect(filter).connect(context.destination);
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+keys.forEach(key => key.addEventListener("click", (e) => mousePlaySound(key, e)));
+window.addEventListener("keydown", keyboardPlaySound);
+
+const controllers = document.querySelectorAll("input[type='range']")
+controllers.forEach(controller => controller.addEventListener("input", () => {
+    appState[controller.name] = controller.value;
+    audioInputs.forEach(audio => audio.playbackRate = appState.playbackRate);
+    filter.frequency.value = appState.filter;
+}));
